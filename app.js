51e8dbcbf412edac61a37ed024b56f3c64e2945d
Russel <russel@dp360crm.com>
2020-04-25 22:17:50 +0600

    admin id

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 require('./config.js');
 
 var nsp_client = io.of('/clients');
-var nsp_master = io.of('/master');
+var nsp_admin = io.of('/admin');
 var nsp_notification = io.of('/notification');
 
+nsp_admin.on('connection', function(socket){
+
+    // console.log( socket.handshake.query );
+    socket.on('disconnect', function () {
+        // console.log('admin disconnected');
+		// io.of('/master').emit('user_removed', new_client );
+    });
+
+    /**
+     * Get List of connected users
+     */
+    socket.on('getUser', function(data){
+        console.log(data);
+        var clients = io.of('/clients').clients();
+        var connected_users = [];
+        for (var id in clients.connected) {
+            var s_client = [];
+            s_client = {
+                'issued' : clients.connected[id].handshake.issued,
+                'socket_id' : id,
+                'user_id' : clients.connected[id].handshake.query.user_id,
+                'branch_id' : clients.connected[id].handshake.query.branch_id,
+                'username' : clients.connected[id].handshake.query.username,
+                'branch_name' : clients.connected[id].handshake.query.branch_name
+            };
+            connected_users.push(s_client);
+        }
+        console.log(connected_users.length);
+        socket.emit('online_user_list', connected_users);
+    });
+
+});
 /**
 * Client Namespace 
 * Handler connection for  users
@@ -34,14 +66,15 @@ nsp_client.on('connection', function(socket){
     // io.of('/clients').emit('new_user_added', new_client );
 
     socket.on('msg', function(data){
-        console.log(data);
+        // console.log(data);
     });
+
+    io.of('/admin').emit('new_user_added', new_client );
     
-    console.log('Connected', new_client);
+    // console.log('Connected', new_client);
 
 	socket.on('disconnect', function () {
-        console.log('disconnected');
-		// io.of('/master').emit('user_removed', new_client );
+        io.of('/admin').emit('user_removed', new_client );
 	});
 
 	
